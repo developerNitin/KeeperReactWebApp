c5d9714140868e97ffc3e3da88df7c919cb34343
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-17 01:48:46 +0530

    appReady

---
+++ src/component/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+    return ( <div className="note">
+        <h1>hello world!</h1>
+        <p>i'm nitin a developer.</p>
+      </div>
+    );
+}
+
+export default Note;
